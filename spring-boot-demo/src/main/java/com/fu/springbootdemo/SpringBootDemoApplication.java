0848bc5db80e177efba333c766aa45ae79681e52
Meta39 <53995537@qq.com>
2025-11-27 11:22:23 +0800

    1.提交忽略文件 2.初始化项目

---
+++ spring-boot-demo/src/main/java/com/fu/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fu.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    static void main() {
+        SpringApplication.run(SpringBootDemoApplication.class);
+    }
+
+}
