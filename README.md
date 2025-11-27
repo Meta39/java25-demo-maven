56e9e51960c2fbdce723f5ea35495b335d316243
Meta39 <53995537@qq.com>
2025-11-27 14:49:11 +0800

    native image 支持

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # java25-demo-maven
 java25-demo-maven
+
+## spring-boot-demo
+### 注意事项
+1.如果在windows需要生成exe，则需要使用 GraalVM JDK 25、 Visual Studio2022及以上
+2.右边 maven 执行顺序mvn clean compile spring-boot:process-aot native:compile -DskipTests
+3.双击 exe 启动，控制台输出start success.表示启动成功
--- spring-boot-demo/src/main/java/com/fu/springbootdemo/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/fu/springbootdemo/SpringBootDemoApplication.java
@@ -1,13 +1,16 @@
 package com.fu.springbootdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
     static void main() {
         SpringApplication.run(SpringBootDemoApplication.class);
+        log.error("start success.");
     }
 
 }
